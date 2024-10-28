1bb1a3da06b0df6284f5875d6a6208900131ea46
AlaskaGLHF <vanushaa89@gmail.com>
2024-10-28 23:52:38 +0300

    add core and db

---
+++ Bank.DataBase/Repository/BankRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bank.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bank.DataBase.Repository
+{
+    public class BankRepository
+    {
+        private readonly BankDbContext _context;
+        public BankRepository(BankDbContext context) 
+        { 
+
+            _context = context;
+
+        }
+            
+        public async Task<List<Card>> GetCards()
+        {
+            var cardEntyties = await _context.Cards
+                .AsNoTracking()
+                .ToListAsync();
+
+
+        }
+
+    }
+}
